47a7ad6ddee0b74ff605bb32202ea7415715f207
toughCircle <toughCircle@gmail.com>
2024-08-05 16:03:48 +0900

    first commit

---
+++ src/test/java/toughcircle/wanted_pre_onboarding_backend/WantedPreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package toughcircle.wanted_pre_onboarding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
