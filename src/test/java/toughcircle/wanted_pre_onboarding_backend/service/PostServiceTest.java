5090aef7c2668f278b370b5f6b7782a8a4fa6c93
toughCircle <toughCircle@gmail.com>
2024-08-06 16:12:35 +0900

    fix: 사용하지 않는 import 제거

--- src/test/java/toughcircle/wanted_pre_onboarding_backend/service/PostServiceTest.java
+++ src/test/java/toughcircle/wanted_pre_onboarding_backend/service/PostServiceTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import toughcircle.wanted_pre_onboarding_backend.model.dto.*;
 import toughcircle.wanted_pre_onboarding_backend.model.entity.Company;
 import toughcircle.wanted_pre_onboarding_backend.model.entity.Post;
